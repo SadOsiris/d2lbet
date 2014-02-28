2d9865e0e9afb1ebc1b11f3dea6543c520fcc247
root <root@localhost.localdomain>
2014-02-28 00:08:06 -0500

    28

--- fan.py
+++ fan.py
@@ -2,6 +2,7 @@
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+import re
 
 def team_ana(str_list):
     strt=str_list.split('\n')
@@ -19,6 +20,9 @@ def team_ana(str_list):
     while (buf<len(content)):
         idx=1
         t=content[buf]
+        #var=re.sub('s+','\t',t).split('\t')
+        #t.strip() for t in data_string.splitlines():
+        #var=t
         var=t.split('\t')
         #print var[0],var[1],var[2],var[3],var[4],var[5],var[6],var[7],var[8],var[9]
         odds=var[8].split()
